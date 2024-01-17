ea74ecb7d93aa99233c4a1f1d82f67bb56801611
Shreeram N S <nsshreeram@gmail.com>
2024-01-17 23:30:33 +0530

    Create Newsitem.js

---
+++ src/components/Newsitem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Newsitem extends Component {
+  render() {
+    return (
+      <div> I'm a Newsitem</div>
+    )
+  }
+}
+
+export default Newsitem
